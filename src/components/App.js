3c3f229f9860681cda46d599f5e70b566ee46c32
ayan4m1 <andrew@bulletlogic.com>
2020-05-08 18:13:36 -0400

    remove Overview page

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 
 import Header from 'components/Header';
 import Control from 'components/Control';
-import Overview from 'components/Overview';
 import Devices from 'components/Devices';
 
 export default function App() {
@@ -12,9 +11,6 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/">
-          <Overview />
-        </Route>
-        <Route exact path="/control">
           <Control />
         </Route>
         <Route exact path="/devices">
